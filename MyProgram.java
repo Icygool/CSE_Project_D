81576ee037e4b3d47a298d6f54d2a81d68f4d7e5
Icygool <123507149+Icygool@users.noreply.github.com>
2024-12-17 09:04:27 -0700

    Update MyProgram.java

--- MyProgram.java
+++ MyProgram.java
@@ -68,5 +68,33 @@ public class MyProgram {
         + "\nEnter Where you would like to go: ");
         String locations1 = scanner.nextLine();
     }
-    
+
+    public static int binarySearch(String[] a1, String str)
+    {
+        int low = 0;
+        int high = a1.length - 1;
+        
+        int indexOf = 0;
+        
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+            int isEqual = a1[mid].compareToIgnoreCase(str);
+            
+            if(a1[mid].equalsIgnoreCase(str))
+            {
+                indexOf = mid;
+                return indexOf;
+            }
+            else if(isEqual <= 0)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return 0;
+    }
 }
