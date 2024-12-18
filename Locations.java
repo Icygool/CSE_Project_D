d0498e0b13303dad08f9d17fbaebe41a5a5e7b29
Icygool <123507149+Icygool@users.noreply.github.com>
2024-12-18 11:07:28 -0700

    Update Locations.java

--- Locations.java
+++ Locations.java
@@ -4,6 +4,7 @@
  */
 package Final_Project;
 
+import java.util.Scanner;
 /**
  *
  * @author 27144004
@@ -23,27 +24,31 @@ public class Locations
 
     public pokemonCenter()
     {
-        System.out.println("Hello, welcome to the pokemon center. would you like to heal your pokemon?: ");
+        Scanner scanner = new Scanner(System.in);
         
-        if()
+        System.out.println("Hello, welcome to the pokemon center. would you like to heal your pokemon? [yes/no]: ");
+        String heal = scanner.nextLine();
+        if(heal.equalsIgnorecase("yes"))
         {
             
         }
+        System.out.printin("After you visit at the pokemon center, you head back to the way you came from.");
         return ;
     }
 
     public forest()
     {
+        System.out.println("As you walk into the forest, the path comes to a halt as the path is blocked off by a"
+                           + " sleeping snorlax, seeing no way around it, you decide to go back.");
         return;
     }
 
-    public rivalBattle()
+    public trainerBattle()
     {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("As you walk into the small town, you see a guy thats standing on the side of the road,"
+                           + "just starin into a wall with a smile on his face, as you walk infront of him he stops you dead in your tracks,"
+                           + "You walked infront of me,  lets battle!");
         return;
-    }
-
-    public moveBack()
-    {
-        return true;
     }
 }
