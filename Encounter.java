00e8f31fb68e8d4ca90c672084060ac6c6102b24
Icygool <123507149+Icygool@users.noreply.github.com>
2024-12-19 09:59:45 -0700

    Update Encounter.java

--- Encounter.java
+++ Encounter.java
@@ -1,1 +1,14 @@
-
+public class Encounter
+{
+  public Pokemon player;
+  public Attacks playerAttack1;
+  public Attacks playerAttack2;
+  public Pokemon opponant;
+  public Attacks opponantAttack1;
+  public Attacks opponantAttack2;
+  
+  public Encounter(Pokemon player, Attacks playerAttack1, Attacks playerAttack2, Pokemon opponant, Attacks opponantAttack1, Attacks opponantAttack2)
+  {
+    
+  }
+}
