251357c8063fa057c66295ffcf1bd20af12fcdfa
Icygool <123507149+Icygool@users.noreply.github.com>
2024-12-27 17:54:20 -0700

    Update Locations.java

--- Locations.java
+++ Locations.java
@@ -22,7 +22,7 @@ public class Locations
         this.opponantName = opponantName;
     }
 
-    public pokemonCenter()
+    public pokemonCenter(Pokemon player)
     {
         Scanner scanner = new Scanner(System.in);
         
@@ -30,17 +30,23 @@ public class Locations
         String heal = scanner.nextLine();
         if(heal.equalsIgnorecase("yes"))
         {
-            
+            player.setHp(player.getMaxHp());
         }
         System.out.printin("After you visit at the pokemon center, you head back to the way you came from.");
         return ;
     }
 
-    public forest()
+    public forest1()
     {
         System.out.println("As you walk into the forest, the path comes to a halt as the path is blocked off by a"
                            + " sleeping snorlax, seeing no way around it, you decide to go back.");
         return;
+    }
+
+    Public forestBattle()
+    {
+        System.out.println("As you walk into the forest, you hear a sound eminating from the tall grass, as you walked closer"
+                           + " a wild bulbasaur pops out and attempts to attack.");
     }
 
     public trainerBattle()
@@ -50,5 +56,11 @@ public class Locations
                            + "just starin into a wall with a smile on his face, as you walk infront of him he stops you dead in your tracks,"
                            + "You walked infront of me,  lets battle!");
         return;
+    }
+
+    public town()
+    {
+        System.out.println("As you walk into town, you see that there is not much to be found, nothing but a couple of "
+                           + " abandoned homes and buildings, seeing nothing interesting, youdecide to go back.");
     }
 }
