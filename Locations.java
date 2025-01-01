224408f0a1217dff51b5235cc909c2bb3a3433d7
Icygool <123507149+Icygool@users.noreply.github.com>
2025-01-01 16:11:53 -0700

    Update Locations.java

--- Locations.java
+++ Locations.java
@@ -22,7 +22,7 @@ public class Locations
         this.opponantName = opponantName;
     }
 
-    public pokemonCenter(Pokemon player)
+    public void pokemonCenter(Pokemon player)
     {
         Scanner scanner = new Scanner(System.in);
         
@@ -34,24 +34,23 @@ public class Locations
             player.getAttack1().resetPP();
             player.getAttack2().resetPP();
         }
-        System.out.printin("After you visit at the pokemon center, you head back to the way you came from.");
-        return ;
+        System.out.println("After you visit at the pokemon center, you head back to the way you came from.");
     }
 
-    public forest1()
+    public void forest1()
     {
         System.out.println("As you walk into the forest, the path comes to a halt as the path is blocked off by a"
                            + " sleeping snorlax, seeing no way around it, you decide to go back.");
         return;
     }
 
-    Public forestBattle()
+    public void forestBattle()
     {
         System.out.println("As you walk into the forest, you hear a sound eminating from the tall grass, as you walked closer"
                            + " a wild bulbasaur pops out and attempts to attack.");
     }
 
-    public trainerBattle()
+    public void trainerBattle()
     {
         Scanner scanner = new Scanner(System.in);
         System.out.println("As you walk into the small town, you see a guy thats standing on the side of the road,"
@@ -60,7 +59,7 @@ public class Locations
         return;
     }
 
-    public town()
+    public void town()
     {
         System.out.println("As you walk into town, you see that there is not much to be found, nothing but a couple of "
                            + " abandoned homes and buildings, seeing nothing interesting, youdecide to go back.");
