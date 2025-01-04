f3f83400f0458be58cd21a70d4658ce892ba800b
Icygool <123507149+Icygool@users.noreply.github.com>
2025-01-04 16:36:43 -0700

    Update Pokemon.java

--- Pokemon.java
+++ Pokemon.java
@@ -18,32 +18,32 @@ public class Pokemon
         this.attack2 = attack2;
     }
 
-    public String getName()
+    public String getName() //gets the name of the pokemon
     {
         return name;
     }
 
-    public Attack getAttack1()
+    public Attack getAttack1() //gets the first attack of the pokemon
     {
         return attack1;
     }
 
-    public Attack getAttack2()
+    public Attack getAttack2() //gets the second attack of the pokemon
     {
         return attack2;
     }
 
-    public int getMaxHp()
+    public int getMaxHp() //gets the maximum hp of the pokemon
     {
         return maxHp;
     }
 
-    public int getHP()
+    public int getHP() //gets the current hp of the pokemon
     {
         return hp;
     }
     
-    public void setHp(int currentHp)
+    public void setHp(int currentHp) //sets the new hp of the pokemon
     {
         hp = currentHp;
         if(hp < 0)
@@ -52,7 +52,7 @@ public class Pokemon
         }
     }
 
-    public boolean isAlive()
+    public boolean isAlive() //checks if the hp is above 0
     {
         return hp < 0;
     }
