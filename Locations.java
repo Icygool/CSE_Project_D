4f3dde42e4a89130c25cc2a2548868961169adf6
Icygool <123507149+Icygool@users.noreply.github.com>
2025-01-04 16:28:12 -0700

    Update Locations.java

--- Locations.java
+++ Locations.java
@@ -2,35 +2,44 @@ import java.util.Scanner;
 
 public class Locations
 {
-    public static void pokemonCenter(Pokemon player)
+    public static void pokemonCenter(Pokemon player, boolean visit) //a place to reset a pokemons stats
     {
         Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Hello, welcome to the pokemon center. would you like to heal your pokemon? [yes/no]: ");
-        String heal = scanner.nextLine();
-        if(heal.equalsIgnorecase("yes"))
+        if(visit == true) //when the player is visiting the pokemon center
+        {
+            System.out.println("Hello, welcome to the pokemon center. would you like to heal your pokemon? [yes/no]: ");
+            String heal = scanner.nextLine();
+            if(heal.equalsIgnorecase("yes"))
+            {
+                player.setHp(player.getMaxHp());
+                player.getAttack1().resetPP();
+                player.getAttack2().resetPP();
+            }
+            System.out.println("After you visit at the pokemon center, you head back to the way you came from.");
+        }
+        else //when the player just lose a pokemon battle
         {
             player.setHp(player.getMaxHp());
             player.getAttack1().resetPP();
             player.getAttack2().resetPP();
+            System.out.println("After healing your pokemon, you head back to hopeful of winning a rematch with your opponant");
         }
-        System.out.println("After you visit at the pokemon center, you head back to the way you came from.");
     }
 
-    public static void forest1()
+    public static void forest1() //a location to be visited in the first section
     {
         System.out.println("As you walk into the forest, the path comes to a halt as the path is blocked off by a"
                            + " sleeping snorlax, seeing no way around it, you decide to go back.");
         return;
     }
 
-    public static void forestBattle()
+    public static void forestBattle() //a battle to be had at the second section
     {
         System.out.println("As you walk into the forest, you hear a sound eminating from the tall grass, as you walked closer"
                            + " a wild pokemon pops out and attempts to attack.");
     }
 
-    public static void trainerBattle()
+    public static void trainerBattle() //a battle to be had at the first section
     {
         System.out.println("As you walk into the small town, you see a guy thats standing on the side of the road,"
                            + "just starin into a wall with a smile on his face, as you walk infront of him he stops you dead in your tracks,"
@@ -38,7 +47,7 @@ public class Locations
         return;
     }
 
-    public static void town()
+    public static void town() //a location that can be visited at the second section
     {
         System.out.println("As you walk into town, you see that there is not much to be found, nothing but a couple of "
                            + " abandoned homes and buildings, seeing nothing interesting, youdecide to go back.");
