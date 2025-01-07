c987973518cefba3628cef1de7ff4c9b909f6a31
Icygool <123507149+Icygool@users.noreply.github.com>
2025-01-07 09:53:31 -0700

    Update Encounter.java

--- Encounter.java
+++ Encounter.java
@@ -4,12 +4,12 @@ import java.util.Random;
 public class Encounter
 {
   public Pokemon player;
-  public Pokemon opponant;
+  public Pokemon opponent;
   
-  public Encounter(Pokemon player, Pokemon opponant)
+  public Encounter(Pokemon player, Pokemon opponent)
   {
     this.player = player;
-    this.opponant = opponant;
+    this.opponent = opponent;
   }
 
   public boolean battle() //the battle sequece
@@ -17,37 +17,39 @@ public class Encounter
     Scanner scanner = new Scanner(System.in);
     Random rand = new Random();
     
-    System.out.println("You sent out " + player.getName() + ", your opponant sent out a " + opponant.getName());
+    System.out.println("You sent out " + player.getName() + ", your opponent sent out a " + opponent.getName());
     boolean victory;
 
-    int currentHpOpponant;
+    int currentHpOpponent;
     int currentHpPlayer;
+    int numHits;
+    int damage;
+    Attacks playerChoice;
+    Attacks opponentChoice = null;
 
     while(true) //repeats until the player or opponant wins
     {
-      currentHpOpponant = opponant.getHP();
-      currentHpPlayer = player.getHP();
+        currentHpOpponent = opponent.getHP();
+        currentHpPlayer = player.getHP();
+        
+        System.out.println("---------------------------------------------------------------------");
       
-        System.out.println(opponant.getName() + " HP:" + currentHpOpponant);
-        System.out.println(player.getName() + " HP: " + currentHpPlayer);
-
-        int numHits;
-        int damage;
-        Attack playerChoice;
-        Attack opponantChoice;
+        System.out.println("Opponants " + opponent.getName() + " HP:" + currentHpOpponent);
+        System.out.println("Your " + player.getName() + " HP: " + currentHpPlayer);     
 
         while(true) //breaks when player enters a proper input
         {
-          System.out.println("What will you like to do?" 
-          + "Opion 1: " + player.Attack1().getName() + ": damage " + " pp:" + player.Attack1().getDamage() + " pp: " + player.Attack1().getPP()
-          + "\n Option2:" + player.Attack2().getName() + ": damage" + " pp: " + player.Attack2().getPP());
+          System.out.println("\nWhat will you like to do?" 
+          + "\n Opion 1: " + player.getAttack1().getName() + "| damage: " + player.getAttack1().getDamage() + "| pp: " + player.getAttack1().getPP() + "| type: " + player.getAttack1().getType()
+          + "\n Option 2: " + player.getAttack2().getName() + "| damage: " + player.getAttack2().getDamage() + " :pp " + player.getAttack2().getPP() + "| type: " + player.getAttack2().getType()
+          + "\n[enter attack name]: ");
           String choice = scanner.nextLine();
           if(choice.equalsIgnoreCase(player.getAttack1().getName()))
           {
             playerChoice = player.getAttack1();
             break;
           }
-          else if(choice.equalsIgnoreCase(player.Attack2().getName()))
+          else if(choice.equalsIgnoreCase(player.getAttack2().getName()))
           {  
             playerChoice = player.getAttack2();
             break;
@@ -63,53 +65,57 @@ public class Encounter
         {
           System.out.println("You do not have enough pp to use this move.");
           numHits = rand.nextInt(5);
-          damage = struggle(opponant, player, numHits);
-          currentHpOpponant -= damage;
+          damage = struggle(opponent, player, numHits);
+          currentHpOpponent -= damage;
         }
         else
         {
-          damage = calculateDamage(opponant, playerChoice);
+          damage = calculateDamage(opponent, playerChoice);
           playerChoice.decreasePP();
           System.out.println("You used " + playerChoice.getName()  
-                              + "dealing " + damage + " damage to the opposing " + opponant.getName() + ".");
-          currentHpOpponant -= damage;
+                              + " dealing " + damage + " damage to the opposing " + opponent.getName() + ".");
+          currentHpOpponent -= damage;
         }
-        opponant.setHp(opponantCurrentHp);
-        if(opponant.isAlive() == false)
+        opponent.setHp(currentHpOpponent);
+        if(opponent.isAlive() == false)
         {
           System.out.println("Congratulations, you win!");
           victory = true;
-            break;
+          break;
         }
             
         int opponantMove = rand.nextInt(2) + 1; //randomly selects a attack for the opponant
         if(opponantMove == 1)
         {
-          opponantChoice = opponant.getAttack1();
+          opponentChoice = opponent.getAttack1();
         }
         else if(opponantMove == 2)
         {
-          opponantChoice = opponant.getAttack2();
+          opponentChoice = opponent.getAttack2();
         }
     
         System.out.println("");
-        if(opponantChoice.isUseable() == false) //opponant attacks
+        if(opponentChoice != null) //chatGPT
         {
-          System.out.println("The opponant does not have pp to use this move. ");
-          numHits = rand.nextInt(5);
-          damage = struggle(player, opponant, numHits);
-          currentHpPlayer -= damage;
+            System.out.println("");
+            if(opponentChoice.isUseable() == false) //opponant attacks
+            {
+                System.out.println("The opponant does not have pp to use this move. ");
+                numHits = rand.nextInt(5) + 1;
+                damage = struggle(player, opponent, numHits);
+                currentHpPlayer -= damage;
+            }
+            else
+            {
+                damage = calculateDamage(player, opponentChoice);
+                opponent.getAttack2().decreasePP();
+                System.out.println("The opponant used " + opponentChoice.getName()  
+                                + " dealing " + damage + " damage to your " + player.getName() + ".");
+                currentHpPlayer -= damage;
+            }
         }
-        else
-        {
-          damage = calculateDamage(player, opponantChoice);
-          opponant.getAttack2().decreasePP();
-          System.out.println("The opponant used " + opponantChoice.getName()  
-                              + "dealing " + damage + " damage to your " + player.getName() + ".");
-          currentHpPlayer -= damage;
-        }
-      
-        player.setHp(currentPlayerHp);
+        
+        player.setHp(currentHpPlayer);
         if(player.isAlive() == false)
         {
           System.out.println("You lose the battle.\n you run to the nearest pokemon center toheal your pokemon.");
@@ -120,39 +126,45 @@ public class Encounter
     return victory;
   }        
 
-  public static int calculateDamage(Pokemon target,Attack attackUsed) //calculates the damage dealt
+  public static int calculateDamage(Pokemon target,Attacks attackUsed) //calculates the damage dealt
   {
-      double typeEffectiveness = 0;
-      if((target.getType().equals("fire") && attackUsed.getType().equals("water"))
-          || (target.getType().equals("water") && attackUsed.getType().equals("grass"))
-          || (target.getType().equals("grass") && attackUsed.getType().equals("fire")))
+      double typeEffectiveness;
+      if((target.getType().equalsIgnoreCase("fire") && attackUsed.getType().equalsIgnoreCase("water"))
+          || ((target.getType().equalsIgnoreCase("water") && attackUsed.getType().equalsIgnoreCase("grass")))
+          || (target.getType().equalsIgnoreCase("grass") && attackUsed.getType().equalsIgnoreCase("fire")))
       {
+          System.out.println("The attack was super effective");
           typeEffectiveness = 2;
       }
-      else if((target.getType().equals("fire") && attackUsed.getType().equals("grass"))
-          || (target.getType().equals("water") && attackUsed.getType().equals("fire"))
-          || (target.getType().equals("grass") && attackUsed.getType().equals("water")))
+      else if(((target.getType().equalsIgnoreCase("fire") && attackUsed.getType().equalsIgnoreCase("grass")))
+          || ((target.getType().equalsIgnoreCase("water") && attackUsed.getType().equalsIgnoreCase("fire")))
+          || (target.getType().equalsIgnoreCase("grass") && attackUsed.getType().equalsIgnoreCase("water")))
       {
+          System.out.println("The attack was not very effective");
           typeEffectiveness = 0.5;
       }
       else
       {
           typeEffectiveness = 1;
       }
-          
-      int damage = attackUsed.getDamage() * typeEffectiveness;
+      
+      double calculatingDamage = attackUsed.getDamage() * typeEffectiveness;
+      int damage = (int)calculatingDamage; //https://www.geeksforgeeks.org/convert-double-to-integer-in-java/
       return damage;
   }
 
   public static int struggle(Pokemon target, Pokemon attacker, int num) //recursion for when the attack has no PP
   {
+      int numOfHits = 0;
+      
       if(num == 0)
       {
           return 0;
       }
-      System.out.println("The " + attacker.getName() + " Struggled against " + target.getName() + ", hitting " + num 
-                         + " time(s), dealing " + (10 * num) + " damage.");
+      numOfHits += 1;
+      System.out.println("The " + attacker.getName() + " Struggled against " + target.getName() + ", hitting " + numOfHits
+                         + " time, dealing " + (10 * numOfHits) + " damage.");
     
-      return 10 * num + struggle(target, attacker, num - 1);
+      return struggle(target, attacker, num - 1) + 10 * numOfHits;
   }
 }
