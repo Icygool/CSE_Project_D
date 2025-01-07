27c98962d78df2b8c784c81da97861362d67b625
Icygool <123507149+Icygool@users.noreply.github.com>
2025-01-07 09:53:59 -0700

    Update Locations.java

--- Locations.java
+++ Locations.java
@@ -9,7 +9,7 @@ public class Locations
         {
             System.out.println("Hello, welcome to the pokemon center. would you like to heal your pokemon? [yes/no]: ");
             String heal = scanner.nextLine();
-            if(heal.equalsIgnorecase("yes"))
+            if(heal.equalsIgnoreCase("yes"))
             {
                 player.setHp(player.getMaxHp());
                 player.getAttack1().resetPP();
@@ -26,11 +26,10 @@ public class Locations
         }
     }
 
-    public static void forest1() //a location to be visited in the first section
+    public static void forest() //a location to be visited in the first section
     {
         System.out.println("As you walk into the forest, the path comes to a halt as the path is blocked off by a"
                            + " sleeping snorlax, seeing no way around it, you decide to go back.");
-        return;
     }
 
     public static void forestBattle() //a battle to be had at the second section
@@ -44,12 +43,11 @@ public class Locations
         System.out.println("As you walk into the small town, you see a guy thats standing on the side of the road,"
                            + "just starin into a wall with a smile on his face, as you walk infront of him he stops you dead in your tracks,"
                            + "You walked infront of me,  lets battle!");
-        return;
     }
 
     public static void town() //a location that can be visited at the second section
     {
         System.out.println("As you walk into town, you see that there is not much to be found, nothing but a couple of "
-                           + " abandoned homes and buildings, seeing nothing interesting, youdecide to go back.");
+                           + " abandoned homes and buildings, seeing nothing interesting, you decide to go back.");
     }
 }
